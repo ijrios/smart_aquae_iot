a662cb273e193e933825543a63b9b4a2ae4edb83
ijrios <joserios055@gmail.com>
2022-11-24 10:59:51 -0500

    Ego sum project upload, laboravi boni.

--- 1_GPIO_finalis.py
+++ 1_GPIO_finalis.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 17 23:28:27 2022
+Created on Thu Nov 18 23:28:27 2022
 
 @author: JOSERIOS3PALACIOS
 
@@ -17,102 +17,97 @@ import pymysql as MySQLdb
 global db
 
 
-#GPIO portubus
+#Satus GPIO
 TRIG = 3
 ECHO = 4
-GPIO.setwarnings(False) #Satus terrores
-GPIO.setmode(GPIO.BCM) #GPIOs sunt initialized
-GPIO.setup(TRIG,GPIO.OUT) #Ultrasound sensorem
-GPIO.setup(ECHO,GPIO.IN) #Ultrasound sensorem
-GPIO.output(TRIG, False) #Ultrasound sensorem
-GPIO.setup(2, GPIO.IN,pull_up_down=GPIO.PUD_UP) #Manualis valvae 
-GPIO.setup(22, GPIO.IN,pull_up_down=GPIO.PUD_UP) #Automatic valvae 
-GPIO.setup(23, GPIO.IN,pull_up_down=GPIO.PUD_UP) #Exinanitio
-GPIO.setup (25, GPIO.OUT) #Implens valvae
-GPIO.setup (26, GPIO.OUT) #Exinanitio valvae
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+GPIO.output(TRIG, False)
+GPIO.setup(2, GPIO.IN,pull_up_down=GPIO.PUD_UP) #Valvae manualis
+GPIO.setup(22, GPIO.IN,pull_up_down=GPIO.PUD_UP) #Valvae automatica
+GPIO.setup(23, GPIO.IN,pull_up_down=GPIO.PUD_UP) #Valvae inanis
+GPIO.setup(11, GPIO.IN,pull_up_down=GPIO.PUD_UP) #Valvae pluvia
+GPIO.setup (25, GPIO.OUT) #Valvae imple
+GPIO.setup (26, GPIO.OUT) #Valvae exinanita
 
-# MYSQL DATABASE - General Occasus
-# Optiones ad database nexu
-hostname = '172.20.34.21'
-username = 'ijrios'
-password = 'verbum_logos'
+#MYSQL DATABASE - General Occasus
+#Optiones ad database nexu
+hostname = '127.0.0.1'
+username = 'pi'
+password = 'raspberry'
 database = 'pidata'
 
-class pluviometer:
-    def __init__(self, rain_pin = 2, sample_time = 10):
-        self.rain_pin = rain_pin
-        self.sample_time = sample_time
-        self.counter = 0
-        self.rain = 0
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.rain_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-        GPIO.add_event_detect(self.rain_pin, GPIO.FALLING, callback = self.count_pulses, bouncetime = 300)
-        t_rain = threading.Thread(target = self.get_rain)
-        t_rain.start()
-    
-    def count_pulses(self, rain_pin):
-        if GPIO.input(self.rain_pin) == False:
-            self.counter = self.counter + 1
-            print("[INFO] counter:{}".format(self.counter))
-            
-            
-    def obtinere_pluvia(self):
-        while True:
-            time.sleep(self.sample_time)
-            self.rain = self.counter *0.2
-            
-            print("[INFO] Rain:{}".format(self.rain))
-            
-def obtinere_distantia():
-    dist_add = 0
+
+print("Expectans sensorem ad habitandum")
+time.sleep(1) #Sedatis tempore
+
+def adepto_distantia(sensorem):
+    dist_addere = 0
     k = 0
     for x in range(20):
+        numerare = 0
         try:
             GPIO.output(TRIG, True)
             time.sleep(0.00001)
             GPIO.output(TRIG, False)
 
             while GPIO.input(ECHO)==0:
-                pulse_start = time.time()
+                pulsus_satus = time.time()
 
             while GPIO.input(ECHO)==1:
-                pulse_end = time.time()
+                pulsus_finis = time.time()
 
-            pulsus_duratione = pulsus_finis - pulsus_satus
-            #conversus distantia
-            distantia = (pulsus_duratione *  34300)/2
-            #nos organize distantia
-            distantia = round(distantia, 3)
-            #nos imprimere distantia
-            print (x, "distantia: ", distantia)
-            dist_add = dist_add + distantia
-            #print "dist_add: ", dist_add
-            time.sleep(.1) # 100ms inter lectiones
+            pulsus_diuturnitas = pulsus_finis - pulsus_satus
 
-        except Exception as e: 
- 
+            distantia = (pulsus_diuturnitas *  34300)/2
+
+            distance = round(distance, 3)
+            print(x, "distantia: ", distantia)
+
+            dist_addere = dist_addere + distantia
+            #print("dist_addere: ", dist_addere)
+             
+            if(sensorem == 1):
+                numerare = numerare+0.1
+                #print("[INFO] Rain:{}".format(pluvia))
+            elif(sensorem == 0):
+                numerare = numerare
+            
+            print(x, "Pluvia: ", numerare)
+            time.sleep(.1) #100ms intervallum inter lectiones
+         
+
+        except Exception as e:
+       
             pass
-        
-    return distantia
-        
-def scribere_firebase():
+       
+    return distantia,numerare
+       
+       
+def legere_firebase():
+
     global db
+
     while True:
+       
+        altitudo_tank = db.get("/Occasum/","Altitudo_Cisternin")
+        print("[INFO] Altitudo tank: {}".format(altitudotank))
+       
+        level_minimum = db.get("/Occasum/","levelminimum")
+        print("[INFO] Minimum planum: {}".format(level_minimum))
+       
+        level_maximum = db.get("/Occasum/","Maximum")
         
-        altura_tanque = db.get("/Configuracion/","Altura_Tanque")
-        print("[INFO] Altura del tanque: {}".format(altura_tanque))
-        
-        nivel_minimo = db.get("/Configuracion/","nivelminimo")
-        print("[INFO] nivel minimo: {}".format(nivel_minimo))
-        
-        nivel_maximo = db.get("/Configuracion/","Maximo")
-        
-        print("[INFO] max level: {}".format(nivel_maximo))
+        tempus = db.get("/Occasum/","Tempus")
+       
+        print("[INFO] Level maximum: {}".format(tempus))
         time.sleep(1)
 
 
-def scribere_MYSQL(identification,timestamp,level,rain):
-    
+def scribe_MYSQL(identification,timestamp,level,rain):
+    print("Scribere database")
     query = "INSERT INTO electronica (identification,timestamp,level,rain) " \
                 "VALUES (%s,%s,%s,%s)"
     args = (identification,timestamp,level,rain)
@@ -124,179 +119,222 @@ def scribere_MYSQL(identification,timestamp,level,rain):
         conn.commit()
 
     except Exception as error:
-        	print(error)
+        print(error)
 
     finally:
-        	cursor.close()
-        	conn.close()
-
-
-def nivel_minimum(dist):
+        cursor.close()
+        conn.close()
     
-    tank_altitudo=120
-    level=tank_altitudo-dist
+def legere_MYSQL():
+
+    try:
+        
+        conn = MySQLdb.connect(host=hostname, user=username, passwd=password, db=database )
+        cursor = conn.cursor()
+        query = ("SELECT * FROM electronica ")
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        
+        print('Total Row(s):', cursor.rowcount)
+        numerare = []
+        for row in rows:
+           numerare.append(row[0])
+
+    except ValueError as e:
+        print(e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+    return numerare
+
+
+def level_minimum(dist):
+   
+    altitudo_tank = 120
+    level = altitudo_tank-dist
     if(level<10):
         time.sleep(2)
         print("-------------------- ")
-        print("Inanis tank: ", level )
+        print("Inanis tank: ", level)
         print("-------------------- ")
         return False  
     else:
-        print("Non est inanis") 
+        print("Non est inanis")
     return True
 
-def nivel_maximum(dist):
-    
-    tank_altitudo=120
-    level=tank_altitudo-dist
+def level_maximum(dist):
+   
+    altitudo_tank = 120
+    level = altitudo_tank-dist
     if(level>115):
         time.sleep(2)
         print("-------------------- ")
-        print("Plena tank: ",   level)
+        print("Plenus tank: ", level)
         print("-------------------- ")
         return False  
     else:
-        print("Non plena") 
+        print("Non plena")
     return True
-       
-print("Expectans sensorem ad habitandum")
-time.sleep(1) #Sedatis tempore
+
+
 
 def main():
-    
+   
     global db
-    cont = 0
-    #Print welcome 
-    print('[{0:s}] starting on {1:s}...'.format("GPIO_Final", datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')))
     #db = firebase.FirebaseApplication("https://digital-7a2c5-default-rtdb.firebaseio.com/")
-
+    cont = 0
+    #Print welcome
+    print('[{0:s}] starting on {1:s}...'.format("GPIO_Final", datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')))
+    
+        
     while True:
-        distantia = obtinere_distantia()
-        print ("distantia: ", distantia)
-        #t_read = threading.Thread(target = read_firebase)
+
+        print("Legere database indices")
+        ultima = legere_MYSQL()
+        print(ultima)
+    
+        if(len(ultima) != 0):
+            tardus = ultima[-1]
+        else:
+            tardus = 0
+
+        input_pluvia = GPIO.input(11) #Sequitur inanis
+        a, b = adepto_distantia(input_pluvia)
+        print ("distantia: ", a)
+        
+        #Database firebase
+        #t_rad = threading.Thread(target = read_firebase)
         #t_sensor = threading.Thread(target = ultrasonic_sensor())
         #t_sensor.start()
         #t_read.start()
-
         pretium = "De custodia"
-        #print("[INFO] Conditio: {}".format(valor))
-        #db.put("/Configuracion/","Estado", valor)
-        
+        #print("[INFO] Conditio: {}".format(pretium))
+        db.put("/Occasum/",Conditio", pretium)
+       
         #---- Inputs et Outputs incipere
-        input_manual = GPIO.input(2) #Manual
+        input_manualis = GPIO.input(2) #Manualis
         input_auto = GPIO.input(22) #Automatic
-        input_vacio = GPIO.input(23) #Sequitur inanis
-        output_llenado = GPIO.input(25) #Imple
-        output_vaciado = GPIO.input(26) #Exinanitio
- 
-        #distancia = ultrasonic_sensor().obtinere_distantia(distantia)
-        minimum = nivel_minimum(distantia)
-        maximun = nivel_maximum(distantia)
+        input_inanis = GPIO.input(23) #Sequi inanis
+        
+        output_imple = GPIO.input(25) #Imple
+        output_exinanita = GPIO.input(26) #Exinanita
+        
+        #distantia = ultrasonic_sensor().adepto_distantia(distantia)
+        minimun = level_minimum(a)
+        maximun = level_maximum(a)
         print("---------------------------")
 
         #---------------------------Automatic modus---------------------------
-        if (input_auto == False and input_manual == True):
+        if (input_auto == False and input_manualis == True):
 
-            print("Automatic modus ")
-            
-            if(minimun == False and maximun == True ):
+            print("------------Automatic modus----------------- ")
+        
+            if(minimun == False and maximun == True):
                 #Inanis est
                 pretium = "Impletio"
-                GPIO.output (25, GPIO.LOW) #Aperit - Repletus - Duxerit volvitur in
-                GPIO.output (26, GPIO.HIGH) #Claudit - Inanis - Duxerit non lucet
-                print("Se abre valvula llenado")
-                print("Llenando")
-                #--------------scribe database----------
+                GPIO.output (25, GPIO.LOW) #Aperit - Repletus - DUXERIT volvitur in
+                GPIO.output (26, GPIO.HIGH) #Claudit - Inanis - DUXERIT non lucet
+                print("Reple valvae opens")
+                print("Impletio")
                 now = datetime.datetime.now()
                 date = now.strftime('%Y-%m-%d %H:%M:%S')
-                cont= cont+1
-                #print("[INFO] Estado: {}".format(valor))
-                #db.put("/Configuracion/","Estado", valor)
-                scribere_MYSQL(str(cont),str(date),format(distance,'.2f'),format(distance,'.2f'))
-                
-        
+                cont= tardus+1
+                #print("[INFO] Conditio: {}".format(pretium))
+                #db.put("/Occasum/",Conditio", pretium)
+                print("Database")
+                write_MYSQL(str(cont),str(date),format(a,'.2f'),str(b))
+                print("Scriptum in database")
+               
+       
             elif(maximun == False and minimun == True ):
                 #Plenum est
+                print("Database")
                 pretium = "Exinanitio"
-                GPIO.output (25, GPIO.HIGH) #Claudit - Inanis - Duxerit non lucet
-                GPIO.output (26, GPIO.LOW) #Aperit - Repletus - Duxerit volvitur in
+                GPIO.output (25, GPIO.HIGH) #Claudit - Inanis - DUXERIT non lucet
+                GPIO.output (26, GPIO.LOW) #Aperit - Repletus - DUXERIT volvitur in
                 print("Exhaurire valvae opens")
                 print("Exinanitio")
-                #--------------scribe database----------
                 now = datetime.datetime.now()
                 date = now.strftime('%Y-%m-%d %H:%M:%S')
-                cont= cont+1
+                cont= tardus+1
                 #print("[INFO] Conditio: {}".format(pretium))
-                #db.put("/Configuracion/","Conditio", pretium)
-                scribere_MYSQL(str(cont),str(date),format(distance,'.2f'),format(distance,'.2f'))
-                
-                
-        #--------------------------Modus manualis---------------------------
-        elif (input_auto == True and input_manual == False):
-          
-            print("Modus manualis ")
+                #db.put("/Occasum/",Conditio", pretium)
+                print("Database")
+                scribe_MYSQL(str(cont),str(date),format(a,'.2f'),str(b))
+                print("Scriptum in database")
+               
+        #---------------------------Modus manualis---------------------------
+
+        elif (input_auto == True and input_manualis == False):
+         
+            print("Modus manualis")
                
             if(minimun == False and maximun == True ):
-                print("Lacus inanis, vis imple")
-                if(input_vacio == False):
+                print("Tank inanis, vis imple")
+                if(input_inanis == False):
                     #Inanis est
                     pretium = "Impletio"
-                    GPIO.output (25, GPIO.LOW) #Aperit - Repletus - Duxerit volvitur in
-                    GPIO.output (26, GPIO.HIGH) #Claudit - Inanis - Duxerit non lucet
+                    GPIO.output (25, GPIO.LOW) #Aperit - Repletus - DUXERIT volvitur in
+                    GPIO.output (26, GPIO.HIGH) #Claudit - Inanis - DUXERIT non lucet
                     print("Imple valvae opens")
                     time.sleep(5)
                     print("-------------------------------")
-                    print("------------Impletio-----------")
+                    print("--------- Impletio ------------")
                     print(".------------------------------")
-                    #--------------scribe database----------
                     now = datetime.datetime.now()
                     date = now.strftime('%Y-%m-%d %H:%M:%S')
-                    cont= cont+1
-                    scribere_MYSQL(str(cont),str(date),format(distance,'.2f'),format(distance,'.2f'))
+                    cont= tardus+1
+                    print("Database")
+                    scribe_MYSQL(str(cont),str(date),format(a,'.2f'),str(b))
+                    print("Scriptum in database")
                 else:
                     print("-------------------------------")
                     print("----- Expectans user ----------")
                     print(".------------------------------")
-                    #--------------scribe database----------
                     now = datetime.datetime.now()
                     date = now.strftime('%Y-%m-%d %H:%M:%S')
-                    cont= cont+1
-                    scribere_MYSQL(str(cont),str(date),format(distance,'.2f'),format(distance,'.2f'))
+                    cont= tardus+1
+                    print("Database")
+                    scribe_MYSQL(str(cont),str(date),format(a,'.2f'),str(b))
+                    print("Scriptum in database")
                     #print("[INFO] Conditio: {}".format(pretium))
-               	    #db.put("/Configuracion/","Conditio", pretium)
-                
+                    #db.put("/Occasum/",Conditio", pretium)
+               
             elif(maximun == False and minimun == True):
-                print("Plena cisternina, vis evacuare")
-                if(input_vacio == True):
+                print("Plena tank, vis evacuare")
+                if(input_inanis == True):
                     #Plenum est
                     pretium = "Exinanitio"
-                    GPIO.output (25, GPIO.HIGH) #Claudit - Inanis - Duxerit non lucet
-                    GPIO.output (26, GPIO.LOW) #Aperit - Repletus - Duxerit volvitur in
+                    GPIO.output (25, GPIO.HIGH) #Claudit - Inanis - DUXERIT non lucet
+                    GPIO.output (26, GPIO.LOW) #Aperit - Repletus - DUXERIT volvitur in
                     print("Exhaurire valvae opens")
                     time.sleep(5)
                     print("-------------------------------")
-                    print("------------Exinanitio-----------")
+                    print("--------- Exinanitio ----------")
                     print(".------------------------------")
-                    #--------------scribe database----------
                     now = datetime.datetime.now()
                     date = now.strftime('%Y-%m-%d %H:%M:%S')
-                    cont= cont+1
-                    scribere_MYSQL(str(cont),str(date),format(distance,'.2f'),format(distance,'.2f'))
+                    cont= tardus+1
+                    print("Database")
+                    scribe_MYSQL(str(cont),str(date),format(a,'.2f'),str(b))
+                    print("Scriptum in database")
                 else:
                     print("-------------------------------")
                     print("------ Expectans user ---------")
                     print(".------------------------------")
-                    #--------------scribe database----------
                     now = datetime.datetime.now()
                     date = now.strftime('%Y-%m-%d %H:%M:%S')
-                    cont= cont+1
-                    scribere_MYSQL(str(cont),str(date),format(distance,'.2f'),format(distance,'.2f'))
-                    #print("[INFO] Conditio: {}".format(valor))
-                    #db.put("/Configuracion/","Conditio", valor)
+                    cont= tardus+1
+                    print("Database")
+                    scribe_MYSQL(str(cont),str(date),format(a,'.2f'),str(b))
+                    print("Scriptum in database")
+                    #print("[INFO] Conditio: {}".format(pretium))
+                    #db.put("/Occasum/",Conditio", pretium)
 
         time.sleep(1)
+        
  
- 
-    
 if __name__ == '__main__':
     main()
--- 2_server_MYSQL.py
+++ 2_server_MYSQL.py
@@ -3,43 +3,76 @@
 Created on Fri Nov 18 20:26:48 2022
 
 @author: JOSERIOS3PALACIOS
-"""
 
+"""
 import datetime
 import pymysql as MySQLdb
 
-# MYSQL DATABASE - General settings
-# Settings for database connection
+#MYSQL DATABASE - General Occasus
+#Optiones ad database nexu
 hostname = '127.0.0.1'
 username = 'pi'
 password = 'raspberry'
 database = 'pidata'
 
-hire_start = datetime.datetime(2022, 11, 19, 3, 50, 00)
-hire_end = datetime.datetime(2022, 11, 19, 4, 5, 30)
 
-def query_with_fetchall():
+def query_cum_fetchall(pretium_satus,pretium_finis):
+
     try:
         
         conn = MySQLdb.connect(host=hostname, user=username, passwd=password, db=database )
         cursor = conn.cursor()
         query = ("SELECT * FROM electronica "
                  "WHERE timestamp BETWEEN %s AND %s")
-        cursor.execute(query,(hire_start, hire_end))
+        cursor.execute(query,(pretium_satus,pretium_finis))
         rows = cursor.fetchall()
 
         print('Total Row(s):', cursor.rowcount)
-        for row in rows:
-            print(row)
-
-    except Error as e:
+    
+    except ValueError as e:
         print(e)
 
     finally:
         cursor.close()
         conn.close()
 
+    return rows
+
 
 if __name__ == '__main__':
-    query_with_fetchall()
 
+    print("Intra latitudinem temporum vis consulere")
+    print("Intra initium date")
+    annus_1 = int(input("Intra annum initium date: "))
+    mensis_1 = int(input("Intra mense initium date: ")) 
+    diem_1 = int(input("Intra diem initium date: "))
+    print("Intra initium temporis")
+    hora_1 = int(input("Intra hora initium date: "))
+    minuta_1 = int(input("Intra minuta initium date: "))
+    seconds_1 = int(input("Intrant ducens seconds: "))
+
+    print("Intra finem date")
+    annus_2 = int(input("Intra annum finalis date: "))
+    mensis_2 = int(input("Intra mense finalis date: ")) 
+    diem_2 = int(input("Intra diem finalis date: "))
+    print("Intra finem temporis")
+    hora_2 = int(input("Intra hora finis date: "))
+    minuta_2 = int(input("Intra minuta finalis date: "))
+    seconds_2 = int(input("Intra seconds finalis date: "))
+    
+    pretium_satus = datetime.datetime(annus_1, mensis_1, diem_1, hora_1, minuta_1, seconds_1)
+    pretium_finis = datetime.datetime(annus_2, mensis_2, diem_2, hora_2, minuta_2, seconds_2)
+    archive = query_cum_fetchall(pretium_satus,pretium_finis)
+    
+    #Scribe archive
+    file = open("test.txt","w")
+    file.writelines("identification,timestamp,level,rain")
+
+    for row in archive:
+        dato = str(row)
+        string = dato.replace("(","").replace(")","")
+        print(string)
+        file.writelines("\n")
+        file.writelines(str(string))
+    print("Generatae file")
+    file.close()
